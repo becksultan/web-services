fca4e0f120d8d8ea1ca92d35432947e247504c95
becksultan <47245739+becksultan@users.noreply.github.com>
2019-10-15 00:49:11 +0600

    Upload Product Service

---
+++ eureka/src/main/java/main/exception/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package main.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(Long id) {
+        super("Could not find product " + id);
+    }
+}
